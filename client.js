ad9fec2117852ca623dd1d33bc458189c9381a7d
Lara Lesar <lara.m.lesar@gmail.com>
2020-11-09 16:58:05 -0600

    pics appending

--- client.js
+++ client.js
@@ -1,1 +1,27 @@
 console.log('Here are all the available people:', people);
+
+$( document ).ready( readyNow );
+
+function readyNow() {
+    console.log('jquery has joined the party');
+    guessWho(people);
+    randomNumber();
+   
+}
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function guessWho(people){
+    for (let i=0; i < people.length; i++){
+        console.log(people[i].githubUsername);
+        //append divs for each one with pic
+        //$('#target').append(`<img src="https://github.com/christopher-black.png?size=250" alt="Profile image of Chris"/>`)
+        let imageUrl= '<img class=picture id ="target${i}" data-person ="${people[i]name}" src="https://github.com/' 
+        let imageUrlTwo = '.png?size=250"/>'
+        $('#target').append(imageUrl+people[i].githubUsername+imageUrlTwo)
+        
+        console.log(imageUrl+people[i].githubUsername+imageUrlTwo);
+       
+    }
+}
